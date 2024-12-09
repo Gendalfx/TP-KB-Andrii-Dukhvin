8017fe32c05acc94652bfdc5812d958a51c1c9b7
Andrii <andreysypertop@gmail.com>
2024-12-09 20:28:45 +0200

    small update test

--- lab_02/test_lab2.py
+++ lab_02/test_lab2.py
@@ -11,19 +11,41 @@ def test_addNewElement(monkeypatch):
     assert test_list[0]["name"] == "Alice"
 
 def test_deleteElement(monkeypatch):
-    inputs = iter(["Alice"])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    add_inputs = iter(["Alice", "0631112222", "alice@example.com", "321 Cedar St"])
+    monkeypatch.setattr('builtins.input', lambda _: next(add_inputs))
 
-    test_list = [{"name": "Alice", "phone": "0631112222", "email": "alice@example.com", "address": "321 Cedar St"}]
+    test_list = []
+    addNewElement(test_list)
+    
+    # Перевірка, що елемент доданий правильно
+    assert len(test_list) == 1
+    assert test_list[0]["name"] == "Alice"
+    
+    # Імітація вводу користувача для видалення елемента
+    delete_inputs = iter(["Alice"])
+    monkeypatch.setattr('builtins.input', lambda _: next(delete_inputs))
+
     deleteElement(test_list)
+    
     assert len(test_list) == 0
+
+
 
 def test_updateElement(monkeypatch):
     inputs = iter(["Alice", "0639998888", "alice_new@example.com", "321 New Cedar St"])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
 
-    test_list = [{"name": "Alice", "phone": "0631112222", "email": "alice@example.com", "address": "321 Cedar St"}]
+    test_list = []
+    addNewElement(test_list)
+
+    assert len(test_list) == 1
+    assert test_list[0]["name"] == "Alice"
+
+    update_inputs = iter(["Alice", "123", "007", "1000"])
+    monkeypatch.setattr('builtins.input', lambda _: next(update_inputs))
+
     updateElement(test_list)
-    assert test_list[0]["phone"] == "0639998888"
-    assert test_list[0]["email"] == "alice_new@example.com"
-    assert test_list[0]["address"] == "321 New Cedar St"
+
+    assert test_list[0]["phone"] == "123"
+    assert test_list[0]["email"] == "007"
+    assert test_list[0]["address"] == "1000"
